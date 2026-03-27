8e4a0674bab6971e48d231af1326dba9fbeb3b91
F4CH <161516529+F4CH@users.noreply.github.com>
2026-03-26 23:17:25 -0300

    Add files via upload

---
+++ Ejercicio 3/src/TareaProcesamiento.java
@@ -0,0 +1,8 @@
+package Ejercicio3;
+
+public class TareaProcesamiento implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Ejecutando tarea en: " + Thread.currentThread().getName());
+    }
+}
