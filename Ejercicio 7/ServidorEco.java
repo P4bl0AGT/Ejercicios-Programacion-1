a50b7f3ef5510f7c7f0e3db9a48750200af67738
F4CH <161516529+F4CH@users.noreply.github.com>
2026-03-27 21:00:39 -0300

    Add files via upload

---
+++ Ejercicio 7/ServidorEco.java
@@ -0,0 +1,20 @@
+package Ejercicio7;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorEco {
+    public static void main(String[] args) {
+        try (ServerSocket servidor = new ServerSocket(5000)) {
+            System.out.println("Servidor de Eco iniciado en puerto 5000...");
+            while (true) {
+                Socket cliente = servidor.accept();
+                System.out.println("Cliente conectado");
+                new Thread(new ManejadorEco(cliente)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
